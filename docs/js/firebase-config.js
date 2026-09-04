bf7af52c02539dc0579b6aa40a13e4dda4b5d4c9
0104gandekan <rt01rw04gandekan@gmail.com>
2026-09-04 23:59:34 +0700

    Update firebase-config.js

--- docs/js/firebase-config.js
+++ docs/js/firebase-config.js
@@ -8,7 +8,7 @@
   };
   g.RT_APP_CONFIG = {
     APP_VERSION: "6.1.2",
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzWZfn4QmQ9aEZS79OJWw9gQstDhPVZDf-t_bJfbZ8NNX6Q1Uho_3dxGqETV2boF7YRJA/exec",
+    APPS_SCRIPT_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnTejqGKGTPa294iyutcyu7VN7snNQchk6X3aeBnxontJ-L_ddIo0ZfVhgKV-t0vy2oNJAAOehELrgiG_IWHQhbi7EYNmE6Q7cTgj8CVmwdfxWILYWsDOt4FBFD2xfljmZ8Cv8E_a8iuY0uukP_DTAZPi4NXiU6a4FY6B4WeVa4acysdaf-37EexfXHkywTlysPoygW7HBGWqs8O6fnnSJh_5VLnYUD64E7ES3HUTZfnS8iLfwBot4vaQHLtgHGTLg4fhOL-xkGLxfznRxDKmFR1NKJzBA&lib=M64ryIPPZT24S0d9E0nRGBOqGhE4ExQAQ",
     FIREBASE_VAPID_KEY: "PASTE_WEB_PUSH_VAPID_KEY",
     ADMIN_BOOTSTRAP_EMAIL: "arjune@gandekan.id",
     PUBLIC_CACHE_KEY: "rt01-v612-public-cache",
