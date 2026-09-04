899cce47a214d2108608733b1e00c73e7f0d99b8
0104gandekan <rt01rw04gandekan@gmail.com>
2026-09-05 00:11:05 +0700

    Update firebase-config.js

--- docs/js/firebase-config.js
+++ docs/js/firebase-config.js
@@ -10,7 +10,7 @@
     APP_VERSION: "6.1.2",
     APPS_SCRIPT_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnTejqGKGTPa294iyutcyu7VN7snNQchk6X3aeBnxontJ-L_ddIo0ZfVhgKV-t0vy2oNJAAOehELrgiG_IWHQhbi7EYNmE6Q7cTgj8CVmwdfxWILYWsDOt4FBFD2xfljmZ8Cv8E_a8iuY0uukP_DTAZPi4NXiU6a4FY6B4WeVa4acysdaf-37EexfXHkywTlysPoygW7HBGWqs8O6fnnSJh_5VLnYUD64E7ES3HUTZfnS8iLfwBot4vaQHLtgHGTLg4fhOL-xkGLxfznRxDKmFR1NKJzBA&lib=M64ryIPPZT24S0d9E0nRGBOqGhE4ExQAQ",
     FIREBASE_VAPID_KEY: "PASTE_WEB_PUSH_VAPID_KEY",
-    ADMIN_BOOTSTRAP_EMAIL: "arjune@gandekan.id",
+    ADMIN_BOOTSTRAP_EMAIL: "rt01rw04gandekan@gmail.com",
     PUBLIC_CACHE_KEY: "rt01-v612-public-cache",
     MAX_IMAGE_MB: 2,
     MAX_DOCUMENT_MB: 8
